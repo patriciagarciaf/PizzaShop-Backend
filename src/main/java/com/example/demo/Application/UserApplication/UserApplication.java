e3900fd266d9d7a2c6a1a31925860ce8e8cf369c
Carji <juanpascualete12@gmail.com>
2021-08-27 09:35:02 +0200

    refactored CORE folder

--- src/main/java/com/example/demo/Application/UserApplication/UserApplication.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Application.UserApplication;
-
-public interface UserApplication {
-
-    public UserDTO addClient(CreateOrUpdateUserDTO dto);
-
-}
--- src/main/java/com/example/demo/Domain/UserDomain/Roles.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Domain.UserDomain;
-
-public enum Roles {
-    USER,
-    EMPLOYEE,
-    ADMIN
-  }
