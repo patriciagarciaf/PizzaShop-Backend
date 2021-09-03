d2d265c0da27988d93a2b6158ec841dfdde3b7d2
patriciagarciaf <88269955+patriciagarciaf@users.noreply.github.com>
2021-09-03 14:53:57 +0200

    fixed commit

--- src/main/java/com/example/demo/application/pizzaApplication/PizzaApplicationImp.java
+++ src/main/java/com/example/demo/application/pizzaApplication/PizzaApplicationImp.java
@@ -3,6 +3,7 @@ package com.example.demo.application.pizzaApplication;
 import java.util.List;
 import java.util.UUID;
 
+import com.example.demo.application.imageApplication.ImageDTO;
 import com.example.demo.application.ingredientApplication.IngredientApplicationImp;
 import com.example.demo.core.ApplicationBase;
 import com.example.demo.domain.imageDomain.Image;
@@ -57,11 +58,13 @@ public class PizzaApplicationImp extends ApplicationBase<Pizza, UUID> implements
         pizza.setPrice(pizza.calculatePrice());
         Image image= new Image();
         image.setId(dto.getImageId());
-        pizza.setImage(image);
         pizza.validate("name", pizza.getName(), (name) -> this.pizzaRepositoryWrite.exists(name));
         this.pizzaRepositoryWrite.add(pizza);
         logger.info(this.serializeObject(pizza, " added."));
-        return this.modelMapper.map(pizza, PizzaDTO.class);
+        ImageDTO imageDTO=this.modelMapper.map(image, ImageDTO.class);
+        PizzaDTO pizzaDTO= this.modelMapper.map(pizza, PizzaDTO.class);
+        pizzaDTO.setImageDTO(imageDTO);
+        return pizzaDTO;
     }
 
     @Override
@@ -90,7 +93,10 @@ public class PizzaApplicationImp extends ApplicationBase<Pizza, UUID> implements
         pizza.setName(dto.getName());
         this.pizzaRepositoryWrite.update(pizza);
         logger.info(this.serializeObject(pizza, " updated"));
-        return this.modelMapper.map(pizza, PizzaDTO.class);
+        PizzaDTO pizzaDTO= this.modelMapper.map(pizza, PizzaDTO.class);
+        ImageDTO imageDTO=this.modelMapper.map(image, ImageDTO.class);
+        pizzaDTO.setImageDTO(imageDTO);
+        return pizzaDTO;
     }
 
     @Override
--- src/main/java/com/example/demo/infrastructure/pizzaInfrastructure/PizzaRepositoryImp.java
+++ src/main/java/com/example/demo/infrastructure/pizzaInfrastructure/PizzaRepositoryImp.java
@@ -1,71 +1,36 @@
 package com.example.demo.infrastructure.pizzaInfrastructure;
 
-import com.cloudinary.Cloudinary;
-import com.example.demo.domain.imageDomain.Image;
-import com.example.demo.domain.pizzaDomain.*;
-import com.example.demo.core.Exceptions.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.example.demo.domain.pizzaDomain.Pizza;
+import com.example.demo.domain.pizzaDomain.PizzaProjection;
+import com.example.demo.domain.pizzaDomain.PizzaRepositoryRead;
+import com.example.demo.domain.pizzaDomain.PizzaRepositoryWrite;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
-import java.util.*;
 
 @Repository
 public class PizzaRepositoryImp implements PizzaRepositoryWrite, PizzaRepositoryRead {
 
     private PizzaRepositoryJPA pizzaRepositoryJPA;
-    private final RedisTemplate<String, byte[]> template;
 
     @Autowired
-    public PizzaRepositoryImp(final PizzaRepositoryJPA pizzaRepositoryJPA,
-            final RedisTemplate<String, byte[]> template) {
+    public PizzaRepositoryImp(final PizzaRepositoryJPA pizzaRepositoryJPA) {
         this.pizzaRepositoryJPA = pizzaRepositoryJPA;
-        this.template = template;
     }
 
     @Override
     public void add(Pizza pizza) {
-        Image image = pizza.getImage();
-        try {
-            byte[] bytes = this.template.opsForValue().get(image.getId().toString());
-            // if (bytes==null) {
-            // }
-            image.setData(bytes);
-            Cloudinary cloudinary = new Cloudinary();
-            String cloudUrl = cloudinary.url().secure(true).publicId(image.getId()).generate();
-            image.setUrl(cloudUrl);
-        } catch (Exception e) {
-            throw new InternalServerErrorException(InternalServerErrorEnum.REDIRECT);
-        } finally {
-            if (!this.template.getConnectionFactory().getConnection().isClosed()) {
-                this.template.getConnectionFactory().getConnection().close();
-            }
-        }
-        pizza.setImage(image);
         this.pizzaRepositoryJPA.save(pizza);
     }
 
     @Override
     public void update(Pizza pizza) {
-        Image image = pizza.getImage();
-        try {
-            byte[] bytes = this.template.opsForValue().get(image.getId().toString());
-            // if (bytes==null) {
-            // }
-            image.setData(bytes);
-            // Cloudinary cloudinary=new Cloudinary();
-            // String cloudUrl=
-            // cloudinary.url().secure(true).publicId(image.getId()).generate();
-            // image.setUrl(cloudUrl);
-        } catch (Exception e) {
-            throw new InternalServerErrorException(InternalServerErrorEnum.REDIRECT);
-        } finally {
-            if (!this.template.getConnectionFactory().getConnection().isClosed()) {
-                this.template.getConnectionFactory().getConnection().close();
-            }
-        }
-        pizza.setImage(image);
         this.pizzaRepositoryJPA.save(pizza);
     }
 
